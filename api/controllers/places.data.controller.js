7164a1b9c23913fa202e88ef4e5489642c389678
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-17 00:17:02 +0300

    Property Image uploading functionality set

--- api/controllers/places.data.controller.js
+++ api/controllers/places.data.controller.js
@@ -1,8 +1,8 @@
 const config = require("../config/config");
 const download = require('image-downloader');
+const fs = require('fs');
 
-
-const upload = async (req,res) => {
+const uploadByLink = async (req,res) => {
 
     const {link} = req.body; 
     const newName = 'photo' + Date.now() + '.jpg'
@@ -16,4 +16,20 @@ const upload = async (req,res) => {
 
 
 
-module.exports = { upload }
+const upload = async (req, res) => {
+    const uploadedFiles = [];
+
+    for (let i=0; i<req.files.length; i++) {
+        const {path, originalname} = req.files[i];
+        const parts= originalname.split('.');
+        const extension = parts[parts.length -1];
+        const newPath = path + '.' + extension;
+
+        fs.renameSync(path, newPath);
+
+        uploadedFiles.push(newPath.replace('uploads/',''))
+    }
+    res.json(uploadedFiles); 
+}
+
+module.exports = { upload, uploadByLink }
--- api/routes/places.routes.js
+++ api/routes/places.routes.js
@@ -1,10 +1,13 @@
 const propertyCtrl = require('../controllers/places.data.controller')
-
+const multer = require('multer');
 const express = require('express');
 const router = express.Router();
 
+const photosMiddleware = multer({dest:'uploads/'})
 
-router.post("/upload-by-link",propertyCtrl.upload);
+
+router.post("/upload-by-link",propertyCtrl.uploadByLink);
+router.post("/upload",photosMiddleware.array('photos', 100),propertyCtrl.upload)
 
 
 module.exports = router;
