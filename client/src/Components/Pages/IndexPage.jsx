f1856f0b435c74c4de4aef64eb6f5888fce4fc88
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-26 00:38:41 +0300

    Add data for places in database

--- client/src/Components/Pages/IndexPage.jsx
+++ client/src/Components/Pages/IndexPage.jsx
@@ -21,10 +21,14 @@ const IndexPage = () => {
                     <div className="bg-gray-500 mb-2  rounded-2xl flex">
                         {place.photos?.[0] && (
                             <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt="" />
-                        )}
+                        )} 
                     </div>
-                    <h2 className="text-sm truncate leading-4">{place.title}</h2>
-                    <h3 className="font-bold leading-4">{place.address}</h3>
+                    <h2 className="font-bold">{place.address}</h2>
+                    <h3 className="text-sm text-gray-500">{place.title}</h3>
+
+                    <div className="mt-2">
+                        <span className="font-bold"> ${place.price}</span> per night
+                    </div>
                 </div>
             })}
         </div>
