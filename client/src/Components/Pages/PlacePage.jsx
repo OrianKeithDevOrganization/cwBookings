3db73e974ca6b17fbc6e93e6f1aeefe1a582e05c
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-26 18:50:39 +0300

    Adding the icons for site

--- client/src/Components/Pages/PlacePage.jsx
+++ client/src/Components/Pages/PlacePage.jsx
@@ -35,7 +35,10 @@ const PlacePage = () => {
                         <h2 className="text-3xl mr-48"> Photos of {place.title} </h2>
 
                         <button  onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black"> 
-                            {/* close icon here */}
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+
                             Close photos
                         </button>
                     </div>
@@ -56,7 +59,11 @@ const PlacePage = () => {
 
             <h1 className="text-3xl">{place.title}</h1>
             <a className="flex gap-1 my-3 block font-semibold underline" target="blank" href={'https://maps.google.com/?q='+place.address}>
-                {/* maps icon  */}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                </svg>
+
                 {place.address}
             </a>
 
@@ -87,7 +94,9 @@ const PlacePage = () => {
                 </div>
 
                 <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500">
-                    {/* photo icon here  */}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
+                    </svg>
                     Show more photos
                 </button>
             </div> 
