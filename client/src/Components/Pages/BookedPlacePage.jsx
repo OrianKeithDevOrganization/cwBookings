e661f2552adf9021623258f3aff495d0fad0ffc4
oriankeith002 <orian.ssemambya@students.mak.ac.ug>
2023-09-26 21:24:40 +0300

    Complete setup of page, gallery component and date component

--- client/src/Components/Pages/BookedPlacePage.jsx
+++ client/src/Components/Pages/BookedPlacePage.jsx
@@ -1,12 +1,47 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import axios from "axios";
+import PlaceAddress from "./PlaceAddress";
+import PlacePhotoGallery from "./PlacePhotoGallery";
+import BookingDates from "./BookingDates";
 
 const BookedPlacePage = () => {
 
     const {id} = useParams();
 
+    const [booking, setBooking] = useState(null);
+
+    useEffect(() => {
+        if (id) {
+            axios.get('/bookings').then(response => {
+                
+                const foundBooking = response.data.find(({_id}) => _id === id)
+                if (foundBooking) {
+                    setBooking(foundBooking)
+                }
+            });
+        }
+    },[id])
+
+    if (!booking) {
+        return '';
+    }
+
     return (
-        <div>
-            single booking: {id} 
+        <div className="my-8">
+            <h1>{booking.place.title}</h1>
+            <PlaceAddress>{booking.place.address}</PlaceAddress>
+            <div className="bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between">
+               <div>
+                    <h2 className="text-2xl mb-4">Your booking information:</h2>
+                    <BookingDates booking={booking} />
+               </div>
+               <div className="bg-primary p-6 text-white rounded-2xl">
+                    <div>Total price</div>
+                    <div className="text-3xl">${booking.price}</div>
+               </div>
+            </div>
+            <PlacePhotoGallery place={booking.place} />
         </div>
     )
 
